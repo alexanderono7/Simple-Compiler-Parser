9bc71804142000bb4e32083495914cfdd47e766a
alexanderono7 <alexanderono7@gmail.com>
2022-11-22 13:58:52 -0700

    segfaulting on execute_program not lexer init

--- parser.cc
+++ parser.cc
@@ -6,3 +6,16 @@
 #include <string.h>
 #include "execute.h"
 #include "lexer.h"
+
+
+LexicalAnalyzer lexer;   // NOTE: even though this file does not peek() at or get() any
+                         // tokens, the very fact that a lexer is declared
+                         // requires that you provide input to the program
+                         // when you execute it even if you don't add any code
+                         // to execute just run ./a.out < test, where test is any
+                         // test case
+
+struct InstructionNode * parse_generate_intermediate_representation()
+{
+    printf("program executed!\n");
+}
